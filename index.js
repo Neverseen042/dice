bb79599b6d437fce7ba87df081963db608c4e544
Neverseen042 <164398055+Neverseen042@users.noreply.github.com>
2024-09-19 11:58:25 +0100

    index.js

--- index.js
+++ index.js
@@ -15,11 +15,16 @@ var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = " Play 1 Wins";
+    document.querySelector("h1").innerHTML = " Player 1 Wins,refresh to play";
 }
 else if ( randomNumber2 > randomNumber1){
-    document.querySelector("h1").innerHTML = "Play 2 wins";
+    document.querySelector("h1").innerHTML = "Player 2 wins,refresh to play";
 }
 else {
-    document.querySelector("h1").innerHTML = "Draw";
+    document.querySelector("h1").innerHTML = "Draw,refresh to play";
+}
+function refreshPage() {
+    location.reload();
+}
+    
 }
